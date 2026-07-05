f9b0ce800037c6d73306c52a96dd24f97b5f667f
EKO ASSOUMOU Jean de Dieu <ekojdd@gmail.com>
2026-07-05 17:02:04 +0100

    design: ajout du favicon de l'application

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,24 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 );
 
-// INTERCEPTION GLOBALE DES APPELS D'API POUR L'APK MOBILE
+// INTERCEPTION GLOBALE DES APPELS D'API POUR L'APK MOBILE & LE WEB
 const ORIGINAL_FETCH = window.fetch;
 window.fetch = function (input, init) {
-  // Si l'appel commence par "/api", on y greffe automatiquement l'IP locale de votre PC
   if (typeof input === 'string' && input.startsWith('/api')) {
-    // ⚠️ REMPLACEZ CETTE IP PAR CELLE DE VOTRE PC (voir Partie 3)
-    const API_BASE = 'http://192.168.1.250:3000'; 
+    
+    // 1. Détecte si le site est ouvert depuis Render en production
+    const isProductionWeb = window.location.hostname.includes('onrender.com');
+
+    // 2. Mettez 'true' pour que l'APK mobile se connecte au serveur Render de production,
+    // ou 'false' si vous développez et testez l'APK sur votre réseau local (192.168.1.250).
+    const FORCE_PRODUCTION_APK = true; 
+
+    // Choix automatique de l'API
+    const API_BASE = (isProductionWeb || FORCE_PRODUCTION_APK)
+      ? 'https://transgabon-connect.onrender.com' // API de Production (Render)
+      : 'http://192.168.1.250:3000';              // API de Développement (Votre PC local)
+
     return ORIGINAL_FETCH(API_BASE + input, init);
   }
   return ORIGINAL_FETCH(input, init);
 };
-
